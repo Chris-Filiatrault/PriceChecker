bf25d451f0376f836f0b387eaae8afc4a4ccdc07
Chris Filiatrault <chris.filiatrault@hotmail.com>
2023-04-19 23:52:56 +1000

    Add timer function

---
+++ PriceCheckerAPI/Functions/CheckPriceTimerFunction.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using PriceCheckerAPI.Services;
+
+namespace PriceCheckerAPI.Functions
+{
+    public class CheckPriceTimerFunction
+    {
+        private readonly IPriceService priceService;
+
+        public CheckPriceTimerFunction(IPriceService priceService)
+        {
+            this.priceService = priceService;
+        }
+
+        [FunctionName("CheckPricesTimerFunction")]
+        public void Run([TimerTrigger("* * 22 * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            try
+            {
+                priceService.CheckPrices(log);
+            }
+            catch (Exception exception)
+            {
+                log.LogError(exception, "Failed to check prices.");
+            }
+        }
+    }
+}
