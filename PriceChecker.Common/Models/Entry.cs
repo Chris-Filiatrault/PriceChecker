422f614394b862f261b80253d896c8d3794dbbde
Chris Filiatrault <chris.filiatrault@hotmail.com>
2023-04-20 23:03:17 +1000

    Create PriceChecker.Common project for models

---
+++ PriceChecker.Common/Models/Entry.cs
@@ -0,0 +1,11 @@
+﻿namespace PriceChecker.Common.Models
+{
+    public class Entry
+    {
+        public int ID { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateOnly Date { get; set; }
+    }
+}
