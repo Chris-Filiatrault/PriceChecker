d50a6b6a7b5e561e16b23a09127243e9e8e0d4c8
Chris Filiatrault <chris.filiatrault@hotmail.com>
2023-06-16 22:26:49 +1000

    Add debug logs

--- src/PriceChecker.Business/Services/PriceService.cs
+++ src/PriceChecker.Business/Services/PriceService.cs
@@ -1,6 +1,7 @@
 ﻿using HtmlAgilityPack;
 using Microsoft.Extensions.Logging;
 using PriceChecker.Business.Services.Interfaces;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PriceChecker.Business.Services
 {
@@ -21,9 +22,9 @@ namespace PriceChecker.Business.Services
             this.log = log;
         }
 
-        public void CheckPrices()
+        public async void CheckPrices()
         {
-            var browniePrice = GetPriceFromElementId(ChocBrownieUrl, ChocBrownieElementId);
+            var browniePrice = await GetPriceFromElementId(ChocBrownieUrl, ChocBrownieElementId);
             log.LogInformation("Price is {BrowniePrice}", browniePrice);
 
             if (browniePrice > 0 && browniePrice < 3)
@@ -32,13 +33,38 @@ namespace PriceChecker.Business.Services
             }
         }
 
-        private decimal GetPriceFromElementId(string url, string elementId)
+        private async Task<decimal> GetPriceFromElementId(string url, string elementId)
         {
             try
             {
+                var httpClient = new HttpClient();
+                var result = await httpClient.GetAsync(url);
+                var content = result.Content;
+                log.LogInformation("Http client content", content);
+
+
+                log.LogInformation("Loading URL", url);
                 HtmlDocument html = client.Load(url);
-                var priceString = html.GetElementbyId(elementId).InnerHtml.Replace("$", string.Empty);
+                log.LogInformation("HTML: {0}", html.Text);
+                
+                HtmlDocument html2 = client.Load(url);
+                log.LogInformation("HTML 2: {0}", html2.Text);
+                
+                HtmlDocument html3 = client.Load(url);
+                log.LogInformation("HTML 3: {0}", html3.Text);
+
+                var element = html.GetElementbyId(elementId);
+                log.LogInformation("Element: {0}", element);
+
+                var innerHTML = element.InnerHtml;
+                log.LogInformation("Inner HTML: {0}", innerHTML);
+
+                var priceString = innerHTML.Replace("$", string.Empty);
+                log.LogInformation($"Price is: {priceString}");
+
                 decimal.TryParse(priceString, out decimal price);
+                log.LogInformation($"Finished parsing, price is {price}");
+                
                 return price;
             }
             catch
